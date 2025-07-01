bcd41cf807ac985b8c065b4c8706572c819268a5
Andreas Sommer <sommer@belsignum.com>
2025-07-01 11:42:32 +0200

    bugfix null coalescing

--- Classes/Userfuncs/ContentTca.php
+++ Classes/Userfuncs/ContentTca.php
@@ -9,11 +9,11 @@ class ContentTca
     public function dynamicTitleByType(&$parameters)
     {
         $ctrl = $parameters['parent']['config']['overrideChildTca']['ctrl'];
-        $labelKey = $ctrl['label'] ?: $GLOBALS['TCA']['tx_booster_domain_model_content']['ctrl']['label'];
+        $labelKey = $ctrl['label'] ?? $GLOBALS['TCA']['tx_booster_domain_model_content']['ctrl']['label'] ?? '';
         $label = $parameters['row'][$labelKey];
-        $parameters['title'] = $ctrl['label_static'] ?: $label;
+        $parameters['title'] = $ctrl['label_static'] ?? $label;
 
-        if ($ctrl['label_alt_force'] && $ctrl['label_static'] === NULL)
+        if (($ctrl['label_alt_force'] ?? null) && $ctrl['label_static'] === NULL)
         {
             $labelAlts = GeneralUtility::trimExplode(',', $ctrl['label_alt']);
             foreach ($labelAlts as $_ => $labelAlt)
