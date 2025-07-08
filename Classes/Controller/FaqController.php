ae42a0f39a81ea41467957b7d466bac5a38bbce7
Andreas Sommer <sommer@belsignum.com>
2025-07-08 12:40:02 +0200

    header layout of accordions is now related to data.header_layout

--- Classes/Controller/FaqController.php
+++ Classes/Controller/FaqController.php
@@ -5,17 +5,18 @@ namespace Belsignum\Booster\Controller;
 use Belsignum\Booster\Domain\Repository\ContentRepository;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class FaqController extends ActionController
 {
     public function __construct(
-        protected readonly ContentRepository $contentRepository
+        protected readonly ContentRepository $contentRepository,
+        protected readonly ContentObjectRenderer $contentObjectRenderer
     ) {}
 
     public function listAction(): ResponseInterface
     {
         $typoScriptFrontendController = $this->request->getAttribute('frontend.controller');
-        $cObj = $typoScriptFrontendController->cObj;
         $pid = $typoScriptFrontendController->id;
         if ($typoScriptFrontendController->getLanguage()->getLanguageId() > 0)
         {
@@ -24,7 +25,7 @@ class FaqController extends ActionController
         $faqs = $this->contentRepository->getFaqsByPid($pid);
         $this->view->assignMultiple([
             'faqs' => $faqs,
-            'data' => $cObj->data
+            'data' => !empty($this->contentObjectRenderer->data) ? $this->contentObjectRenderer->data : $this->configurationManager->getContentObject()->data
         ]);
         return $this->htmlResponse();
     }
